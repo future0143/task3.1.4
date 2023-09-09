4a6c991f2f3fae899487eef5f7965f2dc30bc63f
evgenia <future0143@mail.ru>
2023-09-09 12:55:10 +0200

    adding BoundaryService.java

---
+++ src/main/java/BoundaryService.java
@@ -0,0 +1,12 @@
+public class BoundaryService {
+
+    public int findMin(int[] marks) {
+        int currentMin = marks[0];
+        for (int mark : marks) {
+            if (currentMin > mark) {
+                currentMin = mark;
+            }
+        }
+        return currentMin;
+    }
+}
